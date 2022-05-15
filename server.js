72dc0ea7da6e9913c4c2c16814366df508146d58
Joseph Farchakh <joseph.farchakh02@gmail.com>
2022-05-15 18:42:57 +0300

    NPM COMMIT

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const app = express()
-var API = []
- app.get('/', function (req, res) {
-   res.send('Hello World')
- })
- parseXlsx('Book1.xlsx').then((excel) => {
-  // excel is an array of arrays
-  excel.forEach(data => {
-    API.push({"name":data.name, "email":data.email,"image":data.image})
-  });
+'use strict';
+const excelToJson = require('convert-excel-to-json');
+const result = excelToJson({
+    sourceFile: 'Book1.xlsx'
 });
+
 app.listen(3000)
