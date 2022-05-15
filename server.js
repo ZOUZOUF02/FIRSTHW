8f87fb0cf8edcaeb341a11f4dbd6efcc30e00a50
Joseph Farchakh <joseph.farchakh02@gmail.com>
2022-05-15 17:53:28 +0300

    COMMIT 1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-
+var API = []
+ app.get('/', function (req, res) {
+   res.send('Hello World')
+ })
+ parseXlsx('Book1.xlsx').then((data) => {
+  // data is an array of arrays
+  data.forEach(element => {
+    API.push({"name":element.name, "email":element.email,"img":element.image})
+  });
+});
 app.listen(3000)
